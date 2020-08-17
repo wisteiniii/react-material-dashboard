4fb92babf8f14b2ef5c66ef99bc21d54ac620b3a
wisteiniii <williamjsteinmetziii@gmail.com>
2020-08-16 22:13:48 -0700

    Build

---
+++ build/precache-manifest.cf4f15cbbdbf6ec490109ce5da1072f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7202862364ec327535973199f7bf84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6510ba3e624580e44033",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "6510ba3e624580e44033",
+    "url": "/static/js/2.af75d202.chunk.js"
+  },
+  {
+    "revision": "8cc075ead67d321cfaced872e23d87fb",
+    "url": "/static/js/2.af75d202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28922a0b17b6db5ae498",
+    "url": "/static/js/main.a302a4f0.chunk.js"
+  },
+  {
+    "revision": "658772e3f1fe0ace5c72",
+    "url": "/static/js/runtime-main.dddc5a34.js"
+  }
+]);

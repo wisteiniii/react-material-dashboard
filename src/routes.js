243f04e8328d185707b720a60a348766d3088d89
wisteiniii <williamjsteinmetziii@gmail.com>
2020-08-16 19:06:01 -0700

    Test

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ import EmployeeListView from 'src/views/employee/EmployeeListView';
 import DashboardView from 'src/views/reports/DashboardView';
 import LoginView from 'src/views/auth/LoginView';
 import NotFoundView from 'src/views/errors/NotFoundView';
-import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
 
@@ -19,7 +18,6 @@ const routes = [
       { path: 'account', element: <AccountView /> },
       { path: 'employees', element: <EmployeeListView /> },
       { path: 'dashboard', element: <DashboardView /> },
-      { path: 'products', element: <ProductListView /> },
       { path: 'settings', element: <SettingsView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
